cdf06383e0a18f256d3294241b151c54b003a62c
Jason Solis <jsolis@gmail.com>
2017-05-13 21:31:41 -0400

    add dynamic ListItems with scrolling events, Icons, and Oink Bot with dummy data

--- components/OinkChat.js
+++ components/OinkChat.js
@@ -12,11 +12,14 @@ import {
   Icon,
   Left,
   List,
+  ListItem,
   Right,
   Text,
   Title,
   View,
 } from 'native-base';
+
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 
@@ -28,7 +31,34 @@ class OinkChat extends Component {
 
     this.state = {
       message: '',
+      scrollViewHeight: 0,
+      inputHeight: 0,
     };
+  }
+
+  onScrollViewLayout = (event) => {
+    const layout = event.nativeEvent.layout;
+    this.setState({
+      scrollViewHeight: layout.height,
+    });
+  }
+
+  onInputLayout = (event) => {
+    const layout = event.nativeEvent.layout;
+    this.setState({
+      inputHeight: layout.height,
+    });
+  }
+
+  scrollToBottom(animate = true) {
+    const { scrollViewHeight, inputHeight } = this.state;
+    const { chatHeight } = this.props;
+
+    const scrollTo = chatHeight - scrollViewHeight + inputHeight;
+
+    if (scrollTo > 0) {
+      this.refs.scroll.scrollToPosition(0, scrollTo, animate)
+    }
   }
 
   render() {
@@ -54,11 +84,39 @@ class OinkChat extends Component {
             ref="scroll"
             onLayout={this.onScrollViewLayout}
           >
-            <Text>Messages here</Text>
+            <List
+              dataArray={[
+                {text: 'hello world', name: 'Oink Bot'},
+                {text: 'oh hello there!', name: 'Jay'},
+                {text: 'Im saying something else', name: 'Ayla'},
+              ]}
+              renderRow={(message) =>
+                <ListItem
+                  icon
+                >
+                  <Left>
+                    {message.name === 'Oink Bot' ?
+                      <MaterialCommunityIcons 
+                        name='pig'
+                        color='#FFA4D0'
+                        size={28}
+                      />
+                    :
+                      <Icon name='person' />
+                    }
+                  </Left>
+                  <Body>
+                    <Text style={styles.chatMessage}>{message.text}</Text>
+                    <Text style={styles.chatName}>{message.name}</Text>
+                  </Body>
+                </ListItem>
+              }
+            />
             <TextInput
               style={styles.chatTextInput}
               autoCapitalize="words"
               placeholder="Write something"
+              onLayout={this.onInputLayout}
               onChangeText={text => this.setState({message: text})}
               value={this.state.name}
             />
@@ -87,6 +145,13 @@ const styles = {
     height: 40,
     alignSelf: 'stretch',
     margin: 10,
+    color: '#333',
+  },
+  chatMessage: {
+
+  },
+  chatName: {
+    fontSize: 12,
     color: '#333',
   },
 };
